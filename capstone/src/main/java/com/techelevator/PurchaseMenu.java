68ff890bf47c066b0f120a64754aec887731f7ec
Mary Schmidt <maryschmidt994@gmail.com>
2022-06-09 11:51:10 -0400

    updated names to match

--- capstone/src/main/java/com/techelevator/PurchaseMenu.java
+++ capstone/src/main/java/com/techelevator/PurchaseMenu.java
@@ -10,19 +10,19 @@ public class PurchaseMenu {
     public void returnToPurchaseMenu() {
     System.out.printf("Current Money Provided: $%s\n",moneyFeed);
     System.out.println("(1) Feed Money\n(2) Select Product\n(3) Finish Transaction");
-    Scanner userInput = new Scanner(System.in);
-    String userChoice = userInput.nextLine();
+    Scanner userScanner = new Scanner(System.in);
+    String userChoice = userScanner.nextLine();
     }
 
     public static void main(String[] args) {
 
         if (userChoice.equals("1")) {
             System.out.println("Enter a whole dollar amount.");
-            String userAmountEntered = userInput.nextLine();
+            String userAmountEntered = userScanner.nextLine();
             BigDecimal moneyEntered = new BigDecimal(userAmountEntered);
             moneyFeed.add(moneyEntered);
 
-            
+
         }
 
 
